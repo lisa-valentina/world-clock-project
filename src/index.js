a3d7068b45ee4c73dd74bd91eaae9dc91d7e6dd3
lisa-valentina <lisa.riedel@gmail.com>
2023-01-20 13:44:28 +0100

    added alerts to each option in select, corrected typo in index

---
+++ src/index.js
@@ -0,0 +1,23 @@
+"It is Saturday, October 8, 2023 2:51 PM in Europe/Paris";
+
+function displayTime(event) {
+  let parisTime = moment()
+    .tz("Europe/Paris")
+    .format("dddd, MMMM D, YYYY hh:mm A");
+  let tokyoTime = moment().tz("Asia/Tokyo").format("LLLL");
+  let sydneyTime = moment().tz("Australia/Sydney").format("LLLL");
+  if (event.target.value.length > 1) {
+    if (event.target.value === "paris") {
+      alert(`It is ${parisTime} in Europe/Paris`);
+    }
+    if (event.target.value === "tokyo") {
+      alert(`It is ${tokyoTime} in Asia/Tokyo`);
+    }
+    if (event.target.value === "sydney") {
+      alert(`It is ${sydneyTime} in Australia/Sydney`);
+    }
+  }
+}
+
+let displayCity = document.querySelector("#world-clock");
+displayCity.addEventListener("change", displayTime);
