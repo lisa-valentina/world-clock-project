e5182034fdb9c59e390ee31ef70f31ee18bcb7fa
lisa-valentina <lisa.riedel@gmail.com>
2023-01-20 18:47:52 +0100

    implement function for timezones

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-"It is Saturday, October 8, 2023 2:51 PM in Europe/Paris";
-
 function displayTime(event) {
   let parisTime = moment()
     .tz("Europe/Paris")
@@ -26,3 +24,38 @@ function displayTime(event) {
 
 let displayCity = document.querySelector("#world-clock");
 displayCity.addEventListener("change", displayTime);
+
+setInterval(function displayAucklandDateAndTime(event) {
+  let displayAuckland = document.querySelector("#auckland");
+  let aucklandTimeElement = displayAuckland.querySelector(".time");
+  let aucklandDateElement = displayAuckland.querySelector(".date");
+  let aucklandTime = moment().tz("Pacific/Auckland");
+  aucklandTimeElement.innerHTML = `${aucklandTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  )}`;
+  aucklandDateElement.innerHTML = `${aucklandTime.format("ddd. MMM. D, YYYY")}`;
+}, 1000);
+
+setInterval(function displayCapetownDateAndTime(event) {
+  let displayCapetown = document.querySelector("#capetown");
+  let capetownTimeElement = displayCapetown.querySelector(".time");
+  let capetownDateElement = displayCapetown.querySelector(".date");
+  let capetownTime = moment().tz("Africa/Windhoek");
+  capetownTimeElement.innerHTML = `${capetownTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  )}`;
+  capetownDateElement.innerHTML = `${capetownTime.format("ddd.MMM. D, YYYY")}`;
+}, 1000);
+
+setInterval(function displayVancouverDateAndTime(event) {
+  let displayVancouver = document.querySelector("#vancouver");
+  let vancouverTimeElement = displayVancouver.querySelector(".time");
+  let vancouverDateElement = displayVancouver.querySelector(".date");
+  let vancouverTime = moment().tz("America/Vanvouver");
+  vancouverTimeElement.innerHTML = `${vancouverTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  )}`;
+  vancouverDateElement.innerHTML = `${vancouverTime.format(
+    "ddd. MMM. D, YYYY"
+  )}`;
+}, 1000);
